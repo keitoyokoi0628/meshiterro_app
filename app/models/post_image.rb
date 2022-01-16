747c370a459cc0cd8733adb774be6b1bb24cef72
EC2 Default User <ec2-user@ip-172-31-92-109.ec2.internal>
2022-01-16 14:39:04 +0000

    [Finish]4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,14 @@ class PostImage < ApplicationRecord
 
   belongs_to :user
 
+  has_many :post_comments, dependent: :destroy
+
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
